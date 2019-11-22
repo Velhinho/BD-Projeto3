102241a9e5bfebb33476ae735a80fea3104e7fcc
Velhinho <goncalo.velhinho420@gmail.com>
2019-11-22 12:38:06 +0000

    schema.sql missing some integrity constraints.

--- entrega/schema.sql
+++ entrega/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE item (
     longitude numeric (8, 6),
     
     PRIMARY KEY(id),
-    FOREIGN KEY(latitude, longitude)
+    FOREIGN KEY (latitude, longitude)
         REFERENCES public_location(latitude, longitude)
 );
 
@@ -37,20 +37,60 @@ CREATE TABLE translation_anomaly (
     lang char(3) NOT NULL REFERENCES anomaly(lang),
     lang2 char(3) NOT NULL,
 
-    FOREIGN KEY(id)
-        REFERENCES anomaly(id)
-    CONSTRAINT overlaping_areas
-        CHECK(area != area2),   
-    CONSTRAINT overlaping_languages
-        CHECK(lang != lang2)
+    FOREIGN KEY (id) REFERENCES anomaly(id)
+    CONSTRAINT overlaping_areas CHECK(area != area2),   
+    CONSTRAINT overlaping_languages CHECK(lang != lang2)
 );
 
 CREATE TABLE duplicate (
     item1 char(36),
     item2 char(36),
 
-    FOREIGN KEY(item1, item2)
-        REFERENCES item(id)
-    CONSTRAINT self_duplicate
-        CHECK(item1 < item2)
+    FOREIGN KEY (item1, item2) REFERENCES item(id)
+    CONSTRAINT self_duplicate CHECK(item1 < item2)
+);
+
+CREATE TABLE user_table (
+    email varchar(254),
+    user_password varchar(254) NOT NULL,
+
+    PRIMARY KEY(email)
+);
+
+CREATE TABLE qualified_user (
+    email varchar(254),
+
+    FOREIGN KEY (email) REFERENCES user_table(email)
+);
+
+CREATE TABLE regular_user (
+    email varchar(254),
+  
+    FOREIGN KEY (email) REFERENCES user_table(email)
+);
+
+CREATE TABLE incident (
+    anomaly_id char(36),
+    item_id char(36),
+    email varchar(254),
+
+    FOREIGN KEY (anomaly_id) REFERENCES anomaly(id)
+    FOREIGN KEY (item_id) REFERENCES item(id)
+    FOREIGN KEY (email) REFERENCES user_table(email)
+);
+
+CREATE TABLE correction_proposal (
+    email varchar(254),
+    nro char(36),
+
+    FOREIGN KEY (email) REFERENCES qualified_user(email)
+);
+
+CREATE TABLE correction (
+    email varchar(254),
+    nro char(36),
+    anomaly_id char(36),
+
+    FOREIGN KEY (email, nro) REFERENCES correction_proposal(email, nro),
+    FOREIGN KEY (anomaly_id) REFERENCES incident(anomaly_id)
 );
