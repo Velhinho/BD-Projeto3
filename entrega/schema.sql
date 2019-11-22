e6c9b010c1206d9c24ba0c03dc77da1d3aed3849
Velhinho <goncalo.velhinho420@gmail.com>
2019-11-22 14:52:04 +0000

    Working on psql. Still missing integrity constraints.

--- entrega/schema.sql
+++ entrega/schema.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 CREATE TABLE public_location (
     latitude numeric(9, 6),
     longitude numeric(8, 6),
@@ -13,7 +15,7 @@ CREATE TABLE item (
     latitude numeric(9, 6),
     longitude numeric (8, 6),
     
-    PRIMARY KEY(id),
+    PRIMARY KEY (id),
     FOREIGN KEY (latitude, longitude)
         REFERENCES public_location(latitude, longitude)
 );
@@ -27,46 +29,61 @@ CREATE TABLE anomaly (
     description_text text NOT NULL,
     has_wording_anomaly boolean NOT NULL,
 
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE translation_anomaly (
     id char(36),
-    area varchar(45) NOT NULL REFERENCES anomaly(area)
     area2 varchar(45) NOT NULL,
-    lang char(3) NOT NULL REFERENCES anomaly(lang),
     lang2 char(3) NOT NULL,
 
+    PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES anomaly(id)
-    CONSTRAINT overlaping_areas CHECK(area != area2),   
-    CONSTRAINT overlaping_languages CHECK(lang != lang2)
+    -- CONSTRAINT overlaping_areas CHECK(area != area2),
+    -- CONSTRAINT overlaping_languages CHECK(lang != lang2)
 );
 
 CREATE TABLE duplicate (
     item1 char(36),
     item2 char(36),
-
-    FOREIGN KEY (item1, item2) REFERENCES item(id)
-    CONSTRAINT self_duplicate CHECK(item1 < item2)
+    
+    PRIMARY KEY (item1, item2),
+    FOREIGN KEY (item1) REFERENCES item(id),
+    FOREIGN KEY (item2) REFERENCES item(id)
+    -- CONSTRAINT self_duplicate CHECK(item1 < item2)
 );
 
 CREATE TABLE user_table (
     email varchar(254),
     user_password varchar(254) NOT NULL,
 
-    PRIMARY KEY(email)
+    PRIMARY KEY (email)    
+    /*
+    CONSTRAINT user_specification 
+        CHECK(email in qualified_user or in regular_user) 
+    */
 );
 
 CREATE TABLE qualified_user (
     email varchar(254),
 
+    PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES user_table(email)
+    /*
+    CONSTRAINT not_in_regular_user
+        CHECK(email not in regular_user)
+    */
 );
 
 CREATE TABLE regular_user (
     email varchar(254),
-  
+
+    PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES user_table(email)
+    /*
+    CONSTRAINT not_in_qualified_user
+        CHECK(email not in qualified_user)
+    */
 );
 
 CREATE TABLE incident (
@@ -74,8 +91,9 @@ CREATE TABLE incident (
     item_id char(36),
     email varchar(254),
 
-    FOREIGN KEY (anomaly_id) REFERENCES anomaly(id)
-    FOREIGN KEY (item_id) REFERENCES item(id)
+    PRIMARY KEY (anomaly_id),
+    FOREIGN KEY (anomaly_id) REFERENCES anomaly(id),
+    FOREIGN KEY (item_id) REFERENCES item(id),
     FOREIGN KEY (email) REFERENCES user_table(email)
 );
 
@@ -83,6 +101,8 @@ CREATE TABLE correction_proposal (
     email varchar(254),
     nro char(36),
 
+    UNIQUE(nro),
+    PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES qualified_user(email)
 );
 
@@ -91,6 +111,10 @@ CREATE TABLE correction (
     nro char(36),
     anomaly_id char(36),
 
-    FOREIGN KEY (email, nro) REFERENCES correction_proposal(email, nro),
+    PRIMARY KEY (email, nro, anomaly_id),
+    FOREIGN KEY (email) REFERENCES correction_proposal(email),
+    FOREIGN KEY (nro) REFERENCES correction_proposal(nro),
     FOREIGN KEY (anomaly_id) REFERENCES incident(anomaly_id)
 );
+
+COMMIT;
