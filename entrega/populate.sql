595f2cbdfe26b36b5b41724badb156a30e0910c2
Velhinho <goncalo.velhinho420@gmail.com>
2019-11-28 10:12:42 +0000

    Added test values

--- entrega/populate.sql
+++ entrega/populate.sql
@@ -1,35 +1,85 @@
-INSERT INTO public_location VALUES (latitude, longitude, location_name);
+BEGIN;
+
+INSERT INTO public_location VALUES (42, 42, 'amadora');
+INSERT INTO public_location VALUES (99, 99, 'taguspark');
+INSERT INTO public_location VALUES (66, 66, 'alameda');
 
 INSERT INTO item VALUES (
-    id,
-    description_text,
-    location_name,
-    latitude,
-    longitude
+    '12345',
+    'TEST',
+    'amadora',
+    42,
+    42
+);
+
+INSERT INTO item VALUES (
+    '11111',
+    'ola',
+    'taguspark',
+    99,
+    99
+);
+
+INSERT INTO item VALUES (
+    '22222',
+    'Hello',
+    'taguspark',
+    99,
+    99
 );
 
 INSERT INTO anomaly VALUES (
-    id,
-    area,
-    image_path,
-    lang,
-    tmstmp,
-    description_text,
-    has_wording_anomaly,
+    '00000',
+    'center',
+    'asd.png',
+    'ENG',
+    timestamp '2019-01-01 00:00:00',
+    'TEST',
+    FALSE
 );
 
-INSERT INTO translation_anomaly VALUES (id, area2, lang2);
+INSERT INTO anomaly VALUES (
+    '31415',
+    'left',
+    'foo.png',
+    'ENG',
+    timestamp '2019-04-05 00:00:00',
+    'YA',
+    TRUE
+);
 
-INSERT INTO duplicate VALUES (item1, item2);
+INSERT INTO anomaly VALUES (
+    '01010',
+    'left',
+    'bar.png',
+    'GER',
+    timestamp '2018-08-08 00:00:00',
+    'Very Bad',
+    TRUE
+);
 
-INSERT INTO user_table VALUES (user_email, user_password);
+INSERT INTO translation_anomaly VALUES ('00000', 'right', 'GER');
 
-INSERT INTO qualified_user VALUES (user_email);
+INSERT INTO user_table VALUES ('super@user.com', 'admin');
 
-INSERT INTO regular_user VALUES (user_email);
+INSERT INTO user_table VALUES ('jose@povinho.com', 'bonitobonito');
 
-INSERT INTO incident VALUES (anomaly_id, item_id, user_email);
+INSERT INTO user_table VALUES ('coca@cola.com', 'pepsi');
 
-INSERT INTO correction_proposal VALUES (user_email);
+INSERT INTO qualified_user VALUES ('jose@povinho.com');
 
-INSERT INTO correction VALUES (user_email, anomaly_id);
+INSERT INTO qualified_user VALUES ('coca@cola.com');
+
+INSERT INTO regular_user VALUES ('super@user.com');
+
+INSERT INTO incident VALUES ('00000', '12345', 'super@user.com');
+
+INSERT INTO incident VALUES ('31415', '11111', 'jose@povinho.com');
+
+INSERT INTO incident VALUES ('01010', '22222', 'jose@povinho.com');
+
+INSERT INTO correction_proposal VALUES ('jose@povinho.com');
+
+INSERT INTO correction VALUES ('jose@povinho.com', '00000');
+
+COMMIT;
